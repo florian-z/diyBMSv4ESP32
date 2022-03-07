55beb520805c7876138dc07c1fe878b43d2dc650
florian-z <lofty045@gmail.com>
2022-03-07 15:18:28 +0100

    eeprom write -> eeprom update

--- ATTINYCellModuleMigrate/ATTINYCellModule/ATTINYCellModule/util/eeprom.c
+++ ATTINYCellModuleMigrate/ATTINYCellModule/ATTINYCellModule/util/eeprom.c
@@ -30,19 +30,19 @@ uint16_t get_config_temp2bcoeff() {
 void set_config(configs_t config, void* value) {
   switch(config) {
     case EEPROM_VOLT_CALIB:
-      eeprom_write_float(&volt_calib, *(float*)value);
+      eeprom_update_float(&volt_calib, *(float*)value);
       break;
     case EEPROM_TEMP1_B_COEFF:
-      eeprom_write_word(&temp1_b_coeff, *(uint16_t*)value);
+      eeprom_update_word(&temp1_b_coeff, *(uint16_t*)value);
       break;
     case EEPROM_TEMP2_B_COEFF:
-      eeprom_write_word(&temp2_b_coeff, *(uint16_t*)value);
+      eeprom_update_word(&temp2_b_coeff, *(uint16_t*)value);
       break;
   }
 }
 
 void clear_config() {
-  eeprom_write_dword((uint32_t*)&volt_calib, UINT32_MAX);
-  eeprom_write_word(&temp1_b_coeff, UINT16_MAX);
-  eeprom_write_word(&temp2_b_coeff, UINT16_MAX);
+  eeprom_update_dword((uint32_t*)&volt_calib, UINT32_MAX);
+  eeprom_update_word(&temp1_b_coeff, UINT16_MAX);
+  eeprom_update_word(&temp2_b_coeff, UINT16_MAX);
 }
--- ATTINYCellModuleMigrate/ATTINYCellModule/ATTINYCellModule/util/eeprom.h
+++ ATTINYCellModuleMigrate/ATTINYCellModule/ATTINYCellModule/util/eeprom.h
@@ -8,6 +8,7 @@ typedef enum configs { EEPROM_VOLT_CALIB, EEPROM_TEMP1_B_COEFF, EEPROM_TEMP2_B_C
 float get_config_voltcalib();
 uint16_t get_config_temp1bcoeff();
 uint16_t get_config_temp2bcoeff();
+
 void set_config(configs_t config, void* value);
 void clear_config();
 
