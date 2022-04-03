ab97f6168cf4ce84f3003168f1dc5d6a0d0beaa9
florian-z <lofty045@gmail.com>
2022-04-03 12:19:46 +0200

    encode/decode hex as 0-9&A-F

--- ATTINYCellModuleMigrate/ATTINYCellModule/ATTINYCellModule/process_messages.c
+++ ATTINYCellModuleMigrate/ATTINYCellModule/ATTINYCellModule/process_messages.c
@@ -73,38 +73,44 @@ uint8_t get_temp2_degC() {
 }
 
 /** Decode / Encode bytes */
+uint8_t decode_nibble(const uint8_t nibble_char) {
+  return nibble_char < 'A' ? nibble_char - '0' : nibble_char + 10 - 'A';
+}
 uint8_t parse_chars_to_byte(const uint8_t* bytes) {
-  uint8_t result = (bytes[0]-'0') * 16;
-  result += bytes[1]-'0';
+  uint8_t result = decode_nibble(bytes[0]) * 16;
+  result += decode_nibble(bytes[1]);
   return result;
 }
 uint16_t parse_chars_to_word(const uint8_t* bytes) {
-  uint16_t result = (bytes[0]-'0') * 4096;
-  result += (bytes[1]-'0') * 256;
-  result += (bytes[2]-'0') * 16;
-  result += (bytes[3]-'0');
+  uint16_t result = decode_nibble(bytes[0]) * 4096;
+  result += decode_nibble(bytes[1]) * 256;
+  result += decode_nibble(bytes[2]) * 16;
+  result += decode_nibble(bytes[3]);
   return result;
 }
+uint8_t encode_nibble(const uint8_t nibble_value) {
+  return nibble_value < 10 ? nibble_value + '0' : nibble_value - 10 + 'A';
+}
 void format_byte_to_chars(uint8_t* bytes, const uint8_t data) {
-  bytes[0] = (data/16)+'0';
-  bytes[1] = (data%16)+'0';
+  bytes[0] = encode_nibble(data/16);
+  bytes[1] = encode_nibble(data%16);
 }
 void format_word_to_chars(uint8_t* bytes, const uint16_t data) {
-  bytes[0] = (data/4096)+'0';
-  bytes[1] = ((data%4096)/256)+'0';
-  bytes[2] = ((data%256)/16)+'0';
-  bytes[3] = (data%16)+'0';
+  bytes[0] = encode_nibble(data/4096);
+  bytes[1] = encode_nibble((data%4096)/256);
+  bytes[2] = encode_nibble((data%256)/16);
+  bytes[3] = encode_nibble(data%16);
 }
 
 void format_dword_to_chars(uint8_t* bytes, const uint32_t data) {
-  bytes[0] = (data/268435456)+'0';
-  bytes[1] = (data%268435456)/16777216+'0';
-  bytes[2] = (data%16777216)/1048576+'0';
-  bytes[3] = (data%1048576)/65536+'0';
-  bytes[4] = (data%65536)/4096+'0';
-  bytes[5] = ((data%4096)/256)+'0';
-  bytes[6] = ((data%256)/16)+'0';
-  bytes[7] = (data%16)+'0';
+  bytes[0] = encode_nibble(data/268435456);
+  bytes[1] = encode_nibble((data%268435456)/16777216);
+  bytes[2] = encode_nibble((data%16777216)/1048576);
+  bytes[3] = encode_nibble((data%1048576)/65536);
+  bytes[4] = encode_nibble((data%65536)/4096);
+  bytes[5] = encode_nibble((data%4096)/256);
+  bytes[6] = encode_nibble((data%256)/16);
+  bytes[7] = encode_nibble(data%16);
 }
 
 /** MSG tools */
